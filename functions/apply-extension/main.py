0cd32aa2e36c81fd9daf1e1044dfd93ea620002f
moreal <dev.moreal@gmail.com>
2019-06-16 20:14:47 +0900

    Update dmsapi and refactor function

--- functions/apply-extension/main.py
+++ functions/apply-extension/main.py
@@ -1,5 +1,3 @@
-import json
-import base64
 import logging
 from dmsapi import DMSSession
 
@@ -7,11 +5,6 @@ from dmsapi import DMSSession
 def apply_extension(data, context):
     logging.info(data)
     logging.info(context)
-    decoded_data = base64.b64decode(data['data']).decode()
-    logging.info(decoded_data)
-
-    data = json.loads(decoded_data)
-    logging.info(str(data))
 
     user_id, user_password = data['user_id'], data['user_password']
     room, seat = data['position']
