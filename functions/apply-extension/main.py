8ad9b351fa63ffd3ad1c2313f7bd82bf61adbd4a
moreal <dev.moreal@gmail.com>
2019-06-16 22:01:55 +0900

    Update codes after understanding pubsub machanism

--- functions/apply-extension/main.py
+++ functions/apply-extension/main.py
@@ -1,3 +1,5 @@
+import base64
+import json
 import logging
 from dmsapi import DMSSession
 
@@ -5,6 +7,8 @@ from dmsapi import DMSSession
 def apply_extension(data, context):
     logging.info(data)
     logging.info(context)
+
+    data = json.loads(base64.b64decode(data['data']).decode('utf-8'))
 
     user_id, user_password = data['user_id'], data['user_password']
     room, seat = data['position']
--- functions/publish-apply-extension-jobs/main.py
+++ functions/publish-apply-extension-jobs/main.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import json
 import base64
@@ -16,4 +17,5 @@ def publish_apply_extension_jobs(data, context):
     )
     docs = db.collection('extensions').get()
     for doc in docs:
-        publisher.publish(topic_name, base64.b64encode(json.dumps(doc.to_dict())))
+        logging.info(f"Publish {doc.to_dict()} to {topic_name}")
+        publisher.publish(topic_name, json.dumps(doc.to_dict()).encode())
